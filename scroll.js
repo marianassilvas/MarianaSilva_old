759d418956c0a5d6b575f1444e2e580f8115d977
marianassilvas <crania.mariana@gmail.com>
2022-01-07 03:57:49 +0000

    ajustesindex

--- scroll.js
+++ scroll.js
@@ -1,5 +1,14 @@
-window.onscroll = function() {scrollRotate()};
+window.onscroll = function () {
+  scrollRotate();
+};
 
 function scrollRotate() {
-  var navimg = document.getElementById("rodaboi");<br/>
-  navimg.style.transform = "rotate("+ (window.pageYOffset/5) + "deg)"}
+  let image = document.getElementById("rodaboi");
+  image.style.transform = "rotate(" + window.pageYOffset/5 + "deg)";
+}
+
+let image = document.getElementById("rodaboi");
+image.onclick = function() {
+  window.location.href = this.id + 'index.html';
+  alert("Flash 9.0.115 is required to enjoy this site.");};
+  
