64dcb2a586010db220523a22c6ffc693de5625ed
marianassilvas <crania.mariana@gmail.com>
2022-01-09 01:55:57 +0000

    responsividade

--- scroll.js
+++ scroll.js
@@ -7,8 +7,3 @@ function scrollRotate() {
   image.style.transform = "rotate(" + window.pageYOffset/5 + "deg)";
 }
 
-let image = document.getElementById("rodaboi");
-image.onclick = function() {
-  window.location.href = this.id + 'index.html';
-  alert("Flash 9.0.115 is required to enjoy this site.");};
-  
